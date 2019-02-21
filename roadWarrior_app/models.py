7280b44410661074fbe3a4f7c06969ed0641f12c
cameron-bret <c.bret@whiskerhead.com>
2019-02-21 16:17:04 -0500

    added test model

--- roadWarrior_app/models.py
+++ roadWarrior_app/models.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
 
 # Create your models here.
+class Artist(models.Model):
+    name = models.CharField(max_length=100)
+    nationality = models.CharField(max_length=100)
+    photo_url = models.TextField()
+
+    def __str__(self):
+        return self.name
