d81faef8d24fce704728ec1431555b7288ea5cae
cameron-bret <c.bret@whiskerhead.com>
2019-02-21 16:59:40 -0500

    updated models

--- roadWarrior_app/models.py
+++ roadWarrior_app/models.py
@@ -3,10 +3,19 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-class Artist(models.Model):
-    name = models.CharField(max_length=100)
-    nationality = models.CharField(max_length=100)
-    photo_url = models.TextField()
+class RV(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    vehicleName = models.CharField(max_length=100)
+    make = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    year = models.CharField(max_length=12)
+    rvClass = models.CharField(max_length=20)
+    origin = models.CharField(max_length=100)
+    image = models.TextField()
 
     def __str__(self):
         return self.name
+
+class Post(models.Model):
+    rv = models.ForeignKey(RV, on_delete=models.CASCADE, related_name='posts')
