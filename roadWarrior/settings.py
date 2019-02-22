75d8f5ea2a68134d0a058fa16e3ceb6adfffec39
cameron-bret <c.bret@whiskerhead.com>
2019-02-22 10:14:42 -0500

    adjusted installed apps

--- roadWarrior/settings.py
+++ roadWarrior/settings.py
@@ -27,6 +27,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 # Application definition
 
@@ -38,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'roadWarrior_app',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
