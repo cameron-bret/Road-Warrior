73abb62fb67152e9b2fee4a709d30d7cdbe27e7a
cameron-bret <c.bret@whiskerhead.com>
2019-02-23 11:08:31 -0500

    react skeleton in place, comment backend needs fixing

--- roadWarrior_app/admin.py
+++ roadWarrior_app/admin.py
@@ -4,6 +4,15 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from .models import RV, Post, Comment
 
-admin.site.register(RV)
-admin.site.register(Post)
-admin.site.register(Comment)
+class RVAdmin(admin.ModelAdmin):
+        list_display = ('username', 'password', 'vehicleName', 'make', 'model', 'year', 'rvClass', 'origin', 'image')
+
+class PostAdmin(admin.ModelAdmin):
+        list_display = ('rv', 'post_image', 'title', 'description')
+
+class CommentAdmin(admin.ModelAdmin):
+        list_display = ("id", "postId", "date", "commentText", "commentVoteCount")
+
+admin.site.register(RV, RVAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
