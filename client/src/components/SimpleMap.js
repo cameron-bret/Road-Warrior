07854fc7e3feba67b038715a5fc272f5cd6705f0
cameron-bret <c.bret@whiskerhead.com>
2019-02-25 11:31:45 -0500

    updated bootstrap dependency and began adding google map api

---
+++ client/src/components/SimpleMap.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({text}) => <div> {text} </div>
+
+class SimpleMap extends Component {
+    static defaultProps = {
+        center: {
+            lat: 59.95,
+            lng: 30.33
+        },
+        zoom: 11
+    }
+  render() {
+    return (
+        <div style = {{ height: '100vh', width: '100$'}}>
+        <GoogleMapReact
+            bootstrapURLKeys={{ key: }}
+            defaultCenter={this.props.center}
+            defaultZoom={this.props.zoom}
+        />
+        <AnyReactComponent
+        lat={59.955413}
+        lng={30.337844}
+        text={"Kreyser Avora"}
+        />
+        </div>
+    )
+  }
+}
+
+export default SimpleMap
