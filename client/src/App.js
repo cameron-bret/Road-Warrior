6ae739e05e52b456033a00b7c8b827f073f0e557
cameron-bret <c.bret@whiskerhead.com>
2019-02-26 18:50:52 -0500

    mid project refactoring

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,5 @@
 import React, { Component } from "react";
 import SinglePostModal from "./components/SinglePostModal";
-import PostList from "./components/PostList";
-import RVList from "./components/RVList";
-import CommentList from "./components/CommentList";
 
 import { BrowserRouter as Router, Switch, Route, } from 'react-router-dom'
 
@@ -12,9 +9,6 @@ import { BrowserRouter as Router, Switch, Route, } from 'react-router-dom'
           <Router>
           <Switch>
             <Route exact path="/" component={SinglePostModal} />
-            <Route exact path="/posts" component={PostList}/>
-            <Route exact path="/rvs" component={RVList}/>
-            <Route exact path="/comments" component={CommentList}/>
           </Switch>
         </Router>
                   )
