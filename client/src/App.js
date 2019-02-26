024b4eaeb92310c18393349701e0d5deb7341eaf
cameron-bret <c.bret@whiskerhead.com>
2019-02-26 18:13:22 -0500

    added routes and components for lists

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
 import SinglePostModal from "./components/SinglePostModal";
+import PostList from "./components/PostList";
+import RVList from "./components/RVList";
+import CommentList from "./components/CommentList";
+
+import { BrowserRouter as Router, Switch, Route, } from 'react-router-dom'
 
     class App extends Component {
       render() {
         return (
-                    <div>
-                      <SinglePostModal/>
-                    </div>
+          <Router>
+          <Switch>
+            <Route exact path="/" component={SinglePostModal} />
+            <Route exact path="/posts" component={PostList}/>
+            <Route exact path="/rvs" component={RVList}/>
+            <Route exact path="/comments" component={CommentList}/>
+          </Switch>
+        </Router>
                   )
       }
     }
--- roadWarrior_app/admin.py
+++ roadWarrior_app/admin.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.contrib import admin
 from .models import RV, Post, Comment
-
 
 admin.site.register(RV)
 admin.site.register(Post)
