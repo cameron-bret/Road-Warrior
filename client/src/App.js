610e9a48f5fb8156cd2d0cba9966353741d9909b
cameron-bret <c.bret@whiskerhead.com>
2019-02-26 13:30:07 -0500

    updated component structure for SinglePostModal

--- client/src/App.js
+++ client/src/App.js
@@ -1,89 +1,13 @@
 import React, { Component } from "react";
-import Modal from "./components/Modal";
-import axios from "axios";
-import SimpleMap from "./components/SimpleMap";
+import SinglePostModal from "./components/SinglePostModal";
 
     class App extends Component {
-      constructor(props) {
-        super(props);
-        this.state = {
-          activeItem: {
-            vehicleName: "",
-            post_image: "",
-            title: "",
-            description: "",
-            date: Date,
-          },
-          postList: []
-        };
-      }
-
-      componentDidMount() {
-        this.refreshList();
-      }
-
-      refreshList = () => {
-        axios
-          .get("/api/posts/")
-          .then(res => this.setState({ postList: res.data }))
-          .catch(err => console.log(err));
-      };
-
-      toggle = () => {
-        this.setState({ modal: !this.state.modal });
-      };
-      handleSubmit = item => {
-        this.toggle();
-        if (item.id) {
-          axios
-            .put(`/api/posts/${item.id}/`, item)
-            .then(res => this.refreshList());
-          return;
-        }
-        axios
-          .post("/api/posts/", item)
-          .then(res => this.refreshList());
-      };
-      handleDelete = item => {
-        axios
-          .delete(`/api/posts/${item.id}`)
-          .then(res => this.refreshList());
-      };
-      createItem = () => {
-        const item = { vehicleName: "", post_image:"", title: "", description: "", date: Date };
-        this.setState({ activeItem: item, modal: !this.state.modal });
-      };
-      editItem = item => {
-        this.setState({ activeItem: item, modal: !this.state.modal });
-      };
       render() {
         return (
-          <main className="content">
-            <h1 className="text-white text-uppercase text-left my-4">Road Warrior</h1>
-            <br></br>
-            <div className="row ">
-              <div className="col-md-6 col-sm-10 mx-auto p-0">
-                <div className="card p-3">
-                  <div className="">
-                    <button onClick={this.createItem} className="btn btn-primary">
-                      New Post
-                    </button>
                     <div>
-                      <SimpleMap/>
+                      <SinglePostModal/>
                     </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            {this.state.modal ? (
-              <Modal
-                activeItem={this.state.activeItem}
-                toggle={this.toggle}
-                onSave={this.handleSubmit}
-              />
-            ) : null}
-          </main>
-        );
+                  )
       }
     }
     export default App;
